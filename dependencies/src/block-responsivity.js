7b3d49abba971b9fea9715555d0e4296ba524787
juniorjmm <josemjunior17@gmail.com>
2022-01-10 13:46:59 -0400

    ajuste script responsividade

--- dependencies/src/block-responsivity.js
+++ dependencies/src/block-responsivity.js
@@ -10,8 +10,22 @@ window.addEventListener('load', () => {
                 width = width.replace('px', '');
                 width = Number(width);
 
-                if (width < 600 && !block.classList.contains('small')) block.classList.add('small');
-                if (width < 200 && !block.classList.contains('very-small')) block.classList.add('very-small');
+                if (width < 600 && !block.classList.contains('small')) {
+                    if (block.classList.contains('very-small')) block.classList.remove('very-small');
+
+                    block.classList.add('small');
+                }
+
+                else if (width < 200 && !block.classList.contains('very-small')) {
+                    if (block.classList.contains('small')) block.classList.remove('small');
+
+                    block.classList.add('very-small');
+                }
+
+                else {
+                    if (block.classList.contains('very-small')) block.classList.remove('very-small');
+                    if (block.classList.contains('small')) block.classList.remove('small');
+                }
             }
         })
     }
