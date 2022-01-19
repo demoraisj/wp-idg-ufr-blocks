d6a268860022a330c0b8b6779e6bf137d84df15b
juniorjmm <josemjunior17@gmail.com>
2022-01-19 14:11:02 -0400

    add collapse-list-card

---
+++ blocks/collapse-list-card/src/edit.js
@@ -0,0 +1,128 @@
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { UFRBlockHeader, UFRSelect, UFRInput, UFRIconPicker, UFRListBuilder, UFRCheckbox } from '../../../components';
+import { Fragment } from 'react';
+import Render from "./render";
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function edit({ attributes, setAttributes, isSelected }) {
+	/**
+	 * Desestruturação dos atributos do bloco registrados em block.json -> "attributes"
+	 */
+	const { position, title, items, icon, collapsed, target } = attributes;
+
+	/**
+	 * Opções para configuração de posição do botão
+	 *
+	 * @type { {label: string, value: string}[] }
+	 */
+	const positioningOptions = [
+		{ label: 'Esquerda', value: 'start' },
+		{ label: 'Centro', value: 'center' },
+		{ label: 'Direita', value: 'end' },
+	];
+
+	/**
+	 * Renderiza o conteúdo. Esconde as configurações do bloco quando ele não está selecionado.
+	 *
+	 * @param { boolean } selected
+	 * @return {JSX.Element} Elemento principal condicional
+	 */
+	function ConditionalMainContentRender(selected) {
+		return selected ? (
+			// Visuzalização quando selecionado
+			<div
+				{...useBlockProps({
+					className: 'edit block-responsive ufr-block-component',
+				})}
+			>
+				<div className="row align-items-center">
+					<div className="col config">
+						<UFRBlockHeader
+							title="Cartão Lista Retrátil"
+							description="Configure a aparenência do Cartão Lista Retrátil abaixo. Outras configurações podem estar disponíveis no menu á direita."
+						/>
+
+						<UFRInput
+							label="Título"
+							value={title}
+							attr="title"
+							setter={setAttributes}
+						/>
+
+						<UFRListBuilder
+							setter={setAttributes}
+							attr="items"
+							items={items}
+						/>
+
+						<UFRIconPicker
+							setter={setAttributes}
+							value={icon}
+							attr="icon"
+						/>
+
+						<UFRCheckbox
+							label="Carregar página com o cartão recolhido."
+							checked={collapsed}
+							attr="collapsed"
+							setter={setAttributes}
+						/>
+
+						<UFRCheckbox
+							label="Abrir links em uma nova página."
+							checked={target === '_blank'}
+							attr="target"
+							valWhenChecked="_blank"
+							valWhenUnchecked="_self"
+							setter={setAttributes}
+						/>
+
+					</div>
+				</div>
+			</div>
+		) : (
+			// Visuzalização quando não selecionado
+			<div
+				{...useBlockProps({
+					className: 'show block-responsive ufr-block-component',
+				})}
+			>
+				<div className="row">
+					<div
+						className={`col-12 d-flex justify-content-${position}`}
+					>
+						<Render attributes={attributes} />
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<Fragment>
+			<InspectorControls key="setting">
+				<div id="ufrControls">
+					<fieldset>
+						<UFRSelect
+							label="Posição Horizontal do Cartão Lista Retrátil"
+							options={positioningOptions}
+							value={position}
+							attr="position"
+							setter={setAttributes}
+						/>
+					</fieldset>
+				</div>
+			</InspectorControls>
+
+			{ConditionalMainContentRender(isSelected)}
+		</Fragment>
+	);
+}
---
+++ blocks/collapse-list-card/src/render.js
@@ -0,0 +1,54 @@
+/**
+ * Componente para renderizar o bloco.
+ * É aplicado em dois locais diferentes em edit.js e um em save.js
+ *
+ * @param {boolean} preview Determina se está em modo preview (bloco isSelected), para renderizar diferente, se necessário
+ * @return {JSX.Element} Renderização do bloco
+ */
+export default function Render({preview, attributes}) {
+	const { collapsed, items, title, icon, target } = attributes;
+
+	const parsedItem = JSON.parse(items);
+
+	return (
+		<div className="br-card w-100">
+			<div className="row heading py-3">
+				<div className="col" />
+
+				<div className="col title text-nowrap text-center font-weight-bold">
+					<i className={`mr-3 ${icon}`} />
+					<span>{title}</span>
+				</div>
+
+				<div className="col arrow d-flex justify-content-end align-items-center">
+					<i className={`float-right mr-3 fas ${collapsed ? 'fa-chevron-down' : 'fa-chevron-up'}`} />
+				</div>
+			</div>
+
+			<div className={`row content mt-2 ${collapsed ? 'closed' : ''}`}>
+				<div className="col">
+					<div className="br-list" role="list">
+						{parsedItem.map((item, index) => (
+							<>
+								<div
+									className="align-items-center br-item py-3"
+									role="listitem"
+									onclick={`window.open('${item.link}', '${target}')`}
+									onauxclick={`window.open('${item.link}', '_blank')`}
+								>
+									<div className="row align-items-center">
+										<div className="col-auto number">{index + 1}</div>
+										<div className="col">{item.text}</div>
+										<div className="col-auto" />
+									</div>
+								</div>
+
+								<span className="br-divider" />
+							</>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
