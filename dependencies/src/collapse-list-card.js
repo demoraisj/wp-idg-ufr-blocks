1937ec57b37535f3b2c1b8bb86fea195a8feb474
José Junior <jose.junior@aluno.ufr.edu.br>
2022-03-07 14:35:54 -0400

    update

--- dependencies/src/collapse-list-card.js
+++ dependencies/src/collapse-list-card.js
@@ -1,21 +1,25 @@
 window.addEventListener('load', () => {
-    const headings = document.querySelectorAll('.wp-block-create-block-ufr-collapse-list-card .heading');
+    function toggleCollapse() {
+        const headings = document.querySelectorAll('.wp-block-create-block-ufr-collapse-list-card .heading');
 
-    headings.forEach((heading) => {
-        heading.addEventListener('click', () => {
-            if (heading.nextElementSibling.classList.contains('closed')) {
-                heading.nextElementSibling.classList.remove('closed');
+        headings.forEach((element) => {
+            if (element.nextElementSibling.classList.contains('closed')) {
+                element.nextElementSibling.classList.remove('closed');
             } else {
-                heading.nextElementSibling.classList.add('closed');
+                element.nextElementSibling.classList.add('closed');
             }
 
-            if (heading.querySelector('.arrow i').classList.contains('fa-chevron-down')) {
-                heading.querySelector('.arrow i').classList.remove('fa-chevron-down');
-                heading.querySelector('.arrow i').classList.add('fa-chevron-up');
+            if (element.querySelector('.arrow i').classList.contains('fa-chevron-down')) {
+                element.querySelector('.arrow i').classList.remove('fa-chevron-down');
+                element.querySelector('.arrow i').classList.add('fa-chevron-up');
             } else {
-                heading.querySelector('.arrow i').classList.remove('fa-chevron-up');
-                heading.querySelector('.arrow i').classList.add('fa-chevron-down');
+                element.querySelector('.arrow i').classList.remove('fa-chevron-up');
+                element.querySelector('.arrow i').classList.add('fa-chevron-down');
             }
-        });
+        })
+    }
+
+    document.querySelectorAll('.wp-block-create-block-ufr-collapse-list-card .heading').forEach((el) => {
+        el.addEventListener('click', toggleCollapse);
     });
 })
